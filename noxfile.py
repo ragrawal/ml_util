797596bdac1a1e5b3f79ec8678122c54cbb10a74
ragrawal <ragrawal@varomoney.com>
2020-11-08 21:31:54 -0800

    replacing tox with nox

---
+++ noxfile.py
@@ -0,0 +1,22 @@
+import nox
+
+
+@nox.session
+def lint(session):
+    session.install('pytest==5.3.5', 'setuptools==45.2',
+                    'wheel==0.34.2', 'flake8==3.7.9',
+                    'numpy==1.18.1', 'pandas==1.0.5')
+    session.install('.')
+    session.run('flake8')
+
+
+@nox.session
+@nox.parametrize('pandas', ['1.0.5', '1.1.0'])
+@nox.parametrize('sklearn_pandas', ['2.0.0', '2.0.3'])
+def tests(session, pandas, sklearn_pandas):
+    session.install('pytest==5.3.5', 'setuptools==45.2',
+                    'wheel==0.34.2', 'flake8==3.7.9',
+                    'numpy==1.18.1', f'pandas=={pandas}',
+                    f'sklearn_pandas=={sklearn_pandas}')
+    session.install('.')
+    session.run('py.test', 'README.rst', 'tests')
