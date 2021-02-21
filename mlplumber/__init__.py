6e0e94597f766ea49d22b81f50a490a6e2d11766
ragrawal <ragrawal@varomoney.com>
2021-02-20 22:01:39 -0800

    trying new version

--- mlplumber/__init__.py
+++ mlplumber/__init__.py
@@ -1,9 +1,4 @@
-from pkg_resources import get_distribution, DistributionNotFound
-try:
-    __version__ = get_distribution(__name__).version
-except DistributionNotFound:
-    # package is not installed
-    pass
+__version__ = "develop"
 
 import logging
 print(__name__)
